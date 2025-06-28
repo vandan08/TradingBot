abea41c3e219ee490fe55fdf49b03376ad476d87
Vandan Sheth <vandanbsheth9@gmail.com>
2025-06-28 15:35:14 +0530

    Project Creation (testing3)

---
+++ future_end_point.py
@@ -0,0 +1,42 @@
+import hmac
+import hashlib
+import base64
+import json
+import time
+import requests
+from datetime import datetime, timedelta
+
+# Enter your API Key and Secret here. If you don't have one, you can generate it from the website.
+key = "c59533f65955dd8ae2cf3a054af838dfd18ccbce81917c17"
+secret = "ae637e28beb87b0dd0238fe47f4c924fcb7fc441263827fdb319ad4ff165a443"
+
+# python3
+secret_bytes = bytes(secret, encoding='utf-8')
+
+# Generating a timestamp
+timeStamp = int(round(time.time() * 1000))
+
+body = {
+        "timestamp": timeStamp , # EPOCH timestamp in seconds
+        "status": "open,filled", # Comma separated statuses as open,filled,cancelled
+        "side": "sell", # buy OR sell
+        "page": "1", #// no.of pages needed
+        "size": "10", #// no.of records needed
+    "margin_currency_short_name": ["INR", "USDT"]
+        }
+
+json_body = json.dumps(body, separators = (',', ':'))
+
+signature = hmac.new(secret_bytes, json_body.encode(), hashlib.sha256).hexdigest()
+
+url = "https://api.coindcx.com/exchange/v1/derivatives/futures/orders"
+
+headers = {
+    'Content-Type': 'application/json',
+    'X-AUTH-APIKEY': key,
+    'X-AUTH-SIGNATURE': signature
+}
+
+response = requests.post(url, data = json_body, headers = headers)
+data = response.json()
+print(json.dumps(data, indent=2))
